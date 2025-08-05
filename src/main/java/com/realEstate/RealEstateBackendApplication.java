95adf5f39eea8ea239afe3d69c91fd3cf98499e2
Oscar Mayor <oscar.mayor.dev@gmail.com>
2025-08-05 22:26:20 +1200

    properties

--- src/main/java/com/realEstate/RealEstateBackendApplication.java
+++ src/main/java/com/realEstate/RealEstateBackendApplication.java
@@ -1,14 +1,21 @@
 package com.realEstate;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RealEstateBackendApplication {
+	@Value("${spring.mail.username:NOT_FOUND}")
+	private static String mailUsername;
+
+	@Value("${spring.mail.password:NOT_FOUND}")
+	private static String mailPassword;
 
 	public static void main(String[] args) {
-
+		System.out.println("MAIL USERNAME: " + mailUsername);
+		System.out.println("MAIL PASSWORD: " + mailPassword); // cuidado, solo para testeo
 		Dotenv dotenv = Dotenv.configure()
 				.ignoreIfMissing()
 				.load();
