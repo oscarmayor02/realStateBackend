17568f5b26b46bdb19b3c383089442748e5ccf44
Oscar Mayor <oscar.mayor.dev@gmail.com>
2025-08-22 13:38:02 +1200

    cambios en messages

--- src/main/java/com/realEstate/service/impl/MessageServiceImpl.java
+++ src/main/java/com/realEstate/service/impl/MessageServiceImpl.java
@@ -3,8 +3,10 @@
     import com.realEstate.dto.ConversationDTO;
     import com.realEstate.dto.UserDTO;
     import com.realEstate.model.Message;
+    import com.realEstate.model.Property;
     import com.realEstate.model.User;
     import com.realEstate.repository.MessageRepository;
+    import com.realEstate.repository.PropertyRepository;
     import com.realEstate.service.MessageService;
     import org.springframework.beans.factory.annotation.Autowired;
     import org.springframework.stereotype.Service;
@@ -18,10 +20,22 @@
         @Autowired
         private MessageRepository messageRepository;
 
+        @Autowired
+        private PropertyRepository propertyRepository;
         // Save message to the database
         @Override
         public Message saveMessage(Message message) {
             return messageRepository.save(message);
+        }
+
+        public boolean canAccessChat(Long user1Id, Long user2Id, Long propertyId) {
+            // El chat es accesible si alguno es el host de la propiedad
+            Property property = propertyRepository.findById(propertyId).orElse(null);
+            if (property == null) return false;
+
+            return property.getHost().getId().equals(user1Id) ||
+                    property.getHost().getId().equals(user2Id) ||
+                    user1Id.equals(user2Id); // si son la misma persona
         }
 
         @Override
